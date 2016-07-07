f2362352652e6de2272bcd2b4de38f346c468c70
Brian McFee <brian.mcfee@nyu.edu>
2016-07-06 23:22:36 -0400

    added melspec

---
+++ pumpp/feature/mel.py
@@ -0,0 +1,31 @@
+
+#!/usr/bin/env python
+
+import numpy as np
+import librosa
+
+from .base import FeatureExtractor
+
+__all__ = ['Mel']
+
+
+class Mel(FeatureExtractor):
+
+    def __init__(self, name, sr, hop_length, n_fft, n_mels, fmax=None):
+
+        super(Mel, self).__init__(name, sr, hop_length)
+
+        self.n_fft = n_fft
+        self.n_mels = n_mels
+        self.fmax = fmax
+
+        self.register('mel', [None, n_mels], np.float32)
+
+    def transform_audio(self, y):
+
+        mel = librosa.feature.melspectrogram(y=y, sr=self.sr, n_fft=self.n_fft,
+                                             hop_length=self.hop_length,
+                                             n_mels=self.n_mels,
+                                             fmax=self.fmax).astype(np.float32)
+
+        return {'mel': mel.T}
