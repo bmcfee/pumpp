2410e063742487fcffb55411cb3fa7ef08b676b1
Brian McFee <brian.mcfee@nyu.edu>
2017-02-20 12:58:50 -0500

    added beat/downbeat inverse transformations

--- tests/test_decode.py
+++ tests/test_decode.py
@@ -37,8 +37,16 @@ def ann_tag():
 def ann_beat():
     ann = jams.Annotation(namespace='beat', duration=10)
 
-    for n, i in enumerate(np.arange(0, 8, 0.5)):
-        ann.append(time=i, duration=0, value=n % 4)
+    # for n, i in enumerate(np.arange(0, 10, 0.5)):
+    #    ann.append(time=i, duration=0, value=1 + (n % 4))
+
+    # Make up two measures of 4/4, plus two pickup beats
+    for t, v in [(0, -2), (0.5, -1),
+                 (1, 1), (1.5, 2), (2, 3), (3, 4),
+                 (3.5, 1), (4, 2), (4.5, 3), (5, 4),
+                 (5.5, 1), (6, 2), (6.5, 3), (7, 4)]:
+
+        ann.append(time=t, duration=0, value=v)
 
     return ann
 
@@ -75,7 +83,19 @@ def test_decode_beat(sr, hop_length, ann_beat):
     tc = pumpp.task.BeatTransformer('beat', sr=sr, hop_length=hop_length)
 
     data = tc.transform_annotation(ann_beat, ann_beat.duration)
-    inverse = tc.inverse(data['beat'], ann_beat.duration)
+    inverse = tc.inverse(data['beat'], duration=ann_beat.duration)
+    data2 = tc.transform_annotation(inverse, ann_beat.duration)
+
+    assert np.allclose(data['beat'], data2['beat'])
+
+
+def test_decode_beat_downbeat(sr, hop_length, ann_beat):
+
+    tc = pumpp.task.BeatTransformer('beat', sr=sr, hop_length=hop_length)
+
+    data = tc.transform_annotation(ann_beat, ann_beat.duration)
+    inverse = tc.inverse(data['beat'], downbeat=data['downbeat'],
+                         duration=ann_beat.duration)
     data2 = tc.transform_annotation(inverse, ann_beat.duration)
 
     assert np.allclose(data['beat'], data2['beat'])
